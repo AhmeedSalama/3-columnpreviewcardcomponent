abbbfbbffef5be03cebe54273543f0299ec6f6a8
AhmeedSalama <57839174+AhmeedSalama@users.noreply.github.com>
2024-01-28 20:33:10 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// function multiply(...numbers){
+//   let result = 0;
+//   for(let j = 0; j < numbers.length; ++j++){
+//       if(typeof numbers[j] === "string"){
+//           continue;
+//       }else{
+//           result = parseInt(numbers[j - 1]) * parseInt(numbers[j]);
+//       }
+//   }
+//   return result;
+// }
+
+
+// console.log(multiply(10, 20)); // 200
+// console.log(multiply("A", 10, 30)); // 300
+// console.log(multiply(100.5, 10, "B")); // 1000
